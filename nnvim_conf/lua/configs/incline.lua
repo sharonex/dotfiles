944f69cf72e9cf8baa410e7bec09975b9362f139
Sharon Avni <sharon@pelanor.io>
2025-08-07 12:22:03 +0300

    better icons for incline diagnostics

--- nnvim_conf/lua/configs/incline.lua
+++ nnvim_conf/lua/configs/incline.lua
@@ -92,7 +92,7 @@ require("incline").setup(
 				end
 
 				local function get_diagnostic_label()
-					local icons = { error = "", warn = "", info = "", hint = "" }
+					local icons = { error = "", warn = "", info = "", hint = "󰌵" }
 					local label = {}
 
 					for severity, icon in pairs(icons) do
